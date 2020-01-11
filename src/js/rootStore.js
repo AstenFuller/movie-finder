41df5d3c3dd221f1f6c9b664ca3fa1b744e89c96
Asten Fuller <astenhfuller@gmail.com>
2020-01-11 10:59:44 -0800

    Added routing functionality

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,11 +1,16 @@
 import { createStore } from 'redux';
 import rootReducer from './rootReducer';
+import { applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise-middleware';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
 /* eslint-disable no-underscore-dangle */
-const rootStore = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const rootStore = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(
+      promise
+  )
+));
 /* eslint-enable */
 
 export default rootStore;
